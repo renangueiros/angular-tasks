053678bcadb5fb84cba3be54d9d19dd035a51f8b
Renan Gueiros <renangueiros@outlook.com>
2018-06-26 11:11:32 -0300

    Adicionados módulos do @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
@@ -27,6 +28,7 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     CoreModule
