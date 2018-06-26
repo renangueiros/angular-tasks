cfff2c28c9c4876f8679887f18ecf713189e3d88
Renan Gueiros <renangueiros@outlook.com>
2018-06-26 08:47:27 -0300

    implementada criação de nova conta

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../core/services/auth.service';
+import { User } from '../core/models/user.model';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  user: User = { email: '', password: '' };
+
+  constructor(
+    public auth: AuthService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  signup(): void {
+    this.auth.signup(this.user).then(
+      () => {
+        this.router.navigate(['/tasks']);
+      },
+      error => {
+        console.log(error);
+      });
   }
 
 }
