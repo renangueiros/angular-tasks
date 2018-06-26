db19425ce8645ba9f1da0b3f13454f01317fdde3
Renan Gueiros <renangueiros@outlook.com>
2018-06-26 08:13:03 -0300

    Adicionada rota de detalhes ou edição da tarefa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,12 @@ import { TasksComponent } from './tasks/tasks.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { TaskCreateComponent } from './task-create/task-create.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 const routes: Routes = [
     { path: 'tasks', component: TasksComponent, canActivate: [AuthGuard] },
     { path: 'tasks/create', component: TaskCreateComponent, canActivate: [AuthGuard] },
+    { path: 'tasks/details', component: TaskDetailComponent, canActivate: [AuthGuard] },
     { path: 'signin', component: SigninComponent, canActivate: [UnauthGuard] },
     { path: 'signup', component: SignupComponent, canActivate: [UnauthGuard] },
     { path: '', redirectTo: 'tasks', pathMatch: 'full' }
