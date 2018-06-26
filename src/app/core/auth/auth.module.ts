3ae0eafb45d7cdc927fee9f6542b7d5d4d94e7d1
Renan Gueiros <renangueiros@outlook.com>
2018-06-26 00:09:43 -0300

    Adicionado módulo de autenticação

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    AuthModule
+    CommonModule
   ],
   declarations: []
 })
-export class CoreModule { }
+export class AuthModule { }
