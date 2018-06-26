9be2834d8ac1a16f2fcd5d74baf165babc375210
Renan Gueiros <renangueiros@outlook.com>
2018-06-26 07:37:20 -0300

    Criados componentes para navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
+import { TasksComponent } from './tasks/tasks.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
