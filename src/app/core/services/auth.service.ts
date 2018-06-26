ef2a3c0cb400917d7eda938c236a3cbb588b762f
Renan Gueiros <renangueiros@outlook.com>
2018-06-26 06:55:43 -0300

    Implementado serviço de autenticação

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { CoreModule } from '../core.module';
+
+import { User } from '../models/user.model';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: CoreModule
 })
 export class AuthService {
 
-  constructor() { }
+  authenticated: Observable<boolean>;
+  uid: Observable<string>;
+
+  constructor(public fireAuth: AngularFireAuth) {
+    this.authenticated = fireAuth.authState.pipe(map((user: firebase.User) => !!user));
+    this.uid = fireAuth.authState.pipe(map((user: firebase.User) => user.uid));
+  }
+
+  signin(user: User): Promise<any> {
+    return this.fireAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+  }
+
+  signup(user: User): Promise<any> {
+    return this.fireAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
+  }
+
+  signout(): Promise<void> {
+    return this.fireAuth.auth.signOut();
+  }
+
 }
