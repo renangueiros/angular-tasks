1f9fefff6350cd05fc4e440910332fb2b9ba9bca
Renan Gueiros <renangueiros@outlook.com>
2018-06-26 10:05:55 -0300

    Implementado envio de emai para redefinição de senha

--- src/app/recover-password/recover-password.component.ts
+++ src/app/recover-password/recover-password.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { AuthService } from '../core/services/auth.service';
+import { User } from '../core/models/user.model';
 
 @Component({
   selector: 'app-recover-password',
   templateUrl: './recover-password.component.html',
   styleUrls: ['./recover-password.component.css']
 })
-export class RecoverPasswordComponent implements OnInit {
+export class RecoverPasswordComponent {
 
-  constructor() { }
+  user: User = { email: '', password: '' };
+  emailSent = false;
 
-  ngOnInit() {
+  constructor(public auth: AuthService) { }
+
+  sendRecoverPasswordEmail(): void {
+    this.auth.sendResetPasswordEmail(this.user).then(() => { this.emailSent = true; });
   }
 
 }
