36beb468355e2118ad2c84a83cf1a528875d13f1
Renan Gueiros <renangueiros@outlook.com>
2018-06-26 07:38:09 -0300

    Reorganizadas importações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
+
+import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
