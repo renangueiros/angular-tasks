678d2e8135061adf6e9702aa3507ed31b6ee0d3e
Renan Gueiros <renangueiros@outlook.com>
2018-06-26 07:39:17 -0300

    Importados componentes das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { UnauthGuard } from './core/guards/unauth.guard';
 
+import { TasksComponent } from './tasks/tasks.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
 const routes: Routes = [
-    { path: 'tasks', component: null, canActivate: [AuthGuard] },
-    { path: 'signin', component: null, canActivate: [UnauthGuard] },
-    { path: 'signup', component: null, canActivate: [UnauthGuard] },
+    { path: 'tasks', component: TasksComponent, canActivate: [AuthGuard] },
+    { path: 'signin', component: SigninComponent, canActivate: [UnauthGuard] },
+    { path: 'signup', component: SignupComponent, canActivate: [UnauthGuard] },
     { path: '', redirectTo: 'tasks', pathMatch: 'full' }
 ];
 
