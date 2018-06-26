a9b767b1871f286d507c255907adeab6bdacbe7c
Renan Gueiros <renangueiros@outlook.com>
2018-06-26 07:58:33 -0300

    Implementada criação de nova tarefa

--- src/app/tasks-create/tasks-create.component.ts
+++ src/app/tasks-create/tasks-create.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { TasksService } from '../core/services/tasks.service';
+import { Task } from '../core/models/task.model';
 
 @Component({
   selector: 'app-tasks-create',
   templateUrl: './tasks-create.component.html',
   styleUrls: ['./tasks-create.component.css']
 })
-export class TasksCreateComponent implements OnInit {
+export class TasksCreateComponent {
 
-  constructor() { }
+  task: Task = { title: '' };
 
-  ngOnInit() {
+  constructor(
+    private tasksService: TasksService,
+    private router: Router
+  ) { }
+
+  saveTask(): void {
+    this.tasksService.createTask(this.task.title).then(() => { this.navToTasks(); });
+  }
+
+  cancel(): void {
+    this.navToTasks();
+  }
+
+  navToTasks(): void {
+    this.router.navigate(['/tasks']);
   }
 
 }
